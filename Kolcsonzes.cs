3f704617e49c18e2f8c8b41b30721c0d2d21b02d
Marci02 <nagymarci.0218@gmail.com>
2025-02-04 12:18:01 +0100

    Add project files.

---
+++ Kolcsonzes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolcsonzoCA
+{
+    internal class Kolcsonzes
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int PeopleCount { get; set; }
+        public int TakeOutHour { get; set; }
+        public int TakeOutMinute { get; set; }
+        public int ReturnHour { get; set; }
+        public int ReturnMinute { get; set; }
+
+        public Kolcsonzes(string sor)
+        {
+            var v = sor.Split(';');
+            Name = v[0];
+            Id = int.Parse(v[1]);
+            Type = v[2];
+            PeopleCount = int.Parse(v[3]);
+            TakeOutHour = int.Parse(v[4]);
+            TakeOutMinute = int.Parse(v[5]);
+            ReturnHour = int.Parse(v[6]);
+            ReturnMinute = int.Parse(v[7]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using KolcsonzoCA;
+using System.Text;
+using System.Xml.Linq;
+
+List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
+
+using (var sr = new StreamReader(@"..\..\..\src\kolcsonzes.txt", Encoding.UTF8))
+{
+    sr.ReadLine();
+    while (!sr.EndOfStream)
+    {
+        kolcsonzesek.Add(new Kolcsonzes(sr.ReadLine()));
+    }
+}
+
+Console.WriteLine($"4. feladat: {kolcsonzesek.Count} db kölcsönzés.");
+
+Console.Write("5. feladat: Adjon meg egy nevet: ");
+var f5 = Console.ReadLine();
+
+bool van = false;
+
+foreach (var item in kolcsonzesek)
+{
+    if (item.Name.ToLower() == f5.ToLower())
+    {
+        Console.WriteLine($"A keresett néven {item.TakeOutHour}:{item.TakeOutMinute} - {item.ReturnHour}:{item.ReturnMinute} bérelt!");
+        van = true;
+    }
+}
+
+
+var f6 = kolcsonzesek.Where(x => x.Name.Contains(',')).ToList();
+
+Console.WriteLine($"6. feladat: Aznap {f6.Count} db külföldi és {kolcsonzesek.Count - f6.Count} db magyar kölcsönzött!");
